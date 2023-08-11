dd6630c05fb4f5d1aacd2243d40679b6ed2d113f
Yure7S <yuresilvaprof@gmail.com>
2023-08-11 15:40:06 -0300

    Mostrando a duração do curso em semestres

--- UniVerseAPI.Application/DTOs/Response/CoursesDTO/CourseResponseDTO.cs
+++ UniVerseAPI.Application/DTOs/Response/CoursesDTO/CourseResponseDTO.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UniVerseAPI.Application.DTOs.Response.BaseResponse;
+using UniVerseAPI.Application.Services;
 using UniVerseAPI.Domain.Enums;
 using UniVerseAPI.Domain.Interface;
 using UniVerseAPI.Infra.Data.Context;
@@ -17,6 +18,7 @@ namespace UniVerseAPI.Application.DTOs.Response.CoursesDTO
     public class CourseResponseDTO
     {
         public string? FullName { get; set; }
+        public int Duration { get; set; }
         public int? Seats { get; set; }
         public int? SpotsAvailable { get; set; }
         public string? ShortDescription { get; set; }
@@ -26,6 +28,7 @@ namespace UniVerseAPI.Application.DTOs.Response.CoursesDTO
         public CourseResponseDTO(Course course)
         {
             FullName = course.FullName;
+            Duration = ((course.EndDate - course.StartDate).Days)/180;
             Seats = course.Seats;
             SpotsAvailable = course.SpotsAvailable;
             ShortDescription = course.ShortDescription;
